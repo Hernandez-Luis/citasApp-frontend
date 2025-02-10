4bf7699c3888f9f202666fb8e1ee9290aa30b75d
EfrenDLD <24mdavid25@gmail.com>
2025-02-10 10:31:43 -0600

    Subiendo cambios el footer se acomodo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -40,27 +40,6 @@ const Home = () => {
           </div>
         </div>
       </div>
-
-       {/* Footer Mejorado */}
-       <footer className="home-footer">
-        <div className="footer-content">
-          <p>© 2025 Universidad de la Sierra Júarez. Todos los derechos reservados.</p>
-          <div className="social-icons">
-            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-              <FaFacebook className="icon facebook" />
-            </a>
-            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-              <FaInstagram className="icon instagram" />
-            </a>
-            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-              <FaTwitter className="icon twitter" />
-            </a>
-            <a href="https://wa.me/1234567890" target="_blank" rel="noopener noreferrer">
-              <FaWhatsapp className="icon whatsapp" />
-            </a>
-          </div>
-        </div>
-      </footer>
     </div>
   );
 };
